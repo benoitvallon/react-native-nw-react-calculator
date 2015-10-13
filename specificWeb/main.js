b3bb14fd1f8ce3c5d7352ded617e6c576d456dc1
Benoit VALLON <benoitvallon@gmail.com>
2015-10-13 18:20:55 -0400

    Adds es6 or the web app

--- specificWeb/main.js
+++ specificWeb/main.js
@@ -1,11 +1,9 @@
 'use strict';
 
-var App = require('../common/components/App');
-var React = require('react');
-var ReactDOM = require('react-dom');
-var ReactRouter = require('react-router');
-var Router = ReactRouter.Router
-var Route = ReactRouter.Route;
+import App from '../common/components/App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router';
 
 // CSS
 require('normalize.css');
